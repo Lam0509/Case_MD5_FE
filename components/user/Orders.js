828757492f0895f3ccd8e91fc76a51d1562f71e7
yen <yenjy8@gmail.com>
2023-01-31 12:57:03 +0700

    table order

--- components/user/Orders.js
+++ components/user/Orders.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import {
-    MDBCol,
-    MDBRow,
-    MDBCardText,
-    MDBCardBody,
-
-} from 'mdb-react-ui-kit';
-import DataTable from "../../components/user/UI/UserProfile/table";
+import Table from "@mui/material/Table";
+import {MDBCardBody, MDBCardText, MDBCol, MDBRow} from "mdb-react-ui-kit";
+import DataTable from "../admin/shares/TableData";
 
 export default function OrdersPage() {
     return (
@@ -18,7 +13,31 @@ export default function OrdersPage() {
             </MDBRow>
             <hr/>
             <MDBRow>
-                <DataTable/>
+                <Table striped bordered hover>
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Status</th>
+                        <th>Date</th>
+                        <th>Shipper</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    </tbody>
+                </Table>
+                {/*<DataTable/>*/}
             </MDBRow>
         </MDBCardBody>
     )
